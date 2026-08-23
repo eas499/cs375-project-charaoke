9c7a304673010e25134d28c8581b7fc597c11006
PrecognizantWaffles <67386467+PrecognizantWaffles@users.noreply.github.com>
2026-08-23 17:50:30 -0400

    song actually plays in time with lyrics :D

--- app/public/lyric.js
+++ app/public/lyric.js
@@ -7,7 +7,6 @@ function displayTopSongs() {
 
 function playSong(song) {
     song["mode"] = "lyric";
-    console.log(song);
     fetch("/select_song", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
@@ -18,14 +17,12 @@ function playSong(song) {
 }
 
 function createSongEntry(song) {
-    console.log(song);
     let song_div = document.createElement("div");
     song_div.class = "song";
     let button1 = document.createElement("button");
     button1.classList.add("song_button");
     button1.textContent = song.spotify_name;
     button1.onclick = function(){playSong(song)};
-    console.log(button1);
     song_div.append(button1);
     //song_div.append(createLeaderboardButton()); //TODO: implement later
     return song_div;
