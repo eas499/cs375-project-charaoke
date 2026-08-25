c0969b2b4c7bc0cc7b7f18518e719caaf097d2c3
Ollie Richard <ollierichard42@gmail.com>
2026-08-25 11:15:09 -0400

    Score lyric

--- app/public/game.js
+++ app/public/game.js
@@ -93,6 +93,11 @@ function displayLyric(lyricIter, displayIter) {
             waterfalls[j].textContent = waterfalls[j-1].textContent;
         }
     }
+
+    let inputBox = document.getElementById("type");
+    let typedLyr = inputBox.value;
+    scoreLyric(lyric, typedLyr);
+    typedLyr = "";
     timerID = setTimeout(() => {
         displayLyric(lyricIter, displayIter);
     }, lyric[0]);
@@ -142,4 +147,111 @@ function parseLyricFile(lrc) {
     }
     durations = getDurations(parseTimeList(timestamps));
     return durations.map((dur, i) => [dur, lyrics[i]]);
+}
+
+function scoreLyric(target, typed) {
+    let lyrScore = (1 - (levenshtein(target, typed) / length(target))) * 100;
+    score += lyrScore;
+    let scoreText = document.getElementById("score");
+    scoreText.innerText = "Score: " + score;
+}
+
+// Source - https://stackoverflow.com/a/35279162
+// Posted by gustf, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-08-23, License - CC BY-SA 3.0
+
+function levenshtein(s, t) {
+    if (s === t) {
+        return 0;
+    }
+    var n = s.length, m = t.length;
+    if (n === 0 || m === 0) {
+        return n + m;
+    }
+    var x = 0, y, a, b, c, d, g, h, k;
+    var p = new Array(n);
+    for (y = 0; y < n;) {
+        p[y] = ++y;
+    }
+
+    for (; (x + 3) < m; x += 4) {
+        var e1 = t.charCodeAt(x);
+        var e2 = t.charCodeAt(x + 1);
+        var e3 = t.charCodeAt(x + 2);
+        var e4 = t.charCodeAt(x + 3);
+        c = x;
+        b = x + 1;
+        d = x + 2;
+        g = x + 3;
+        h = x + 4;
+        for (y = 0; y < n; y++) {
+            k = s.charCodeAt(y);
+            a = p[y];
+            if (a < c || b < c) {
+                c = (a > b ? b + 1 : a + 1);
+            }
+            else {
+                if (e1 !== k) {
+                    c++;
+                }
+            }
+
+            if (c < b || d < b) {
+                b = (c > d ? d + 1 : c + 1);
+            }
+            else {
+                if (e2 !== k) {
+                    b++;
+                }
+            }
+
+            if (b < d || g < d) {
+                d = (b > g ? g + 1 : b + 1);
+            }
+            else {
+                if (e3 !== k) {
+                    d++;
+                }
+            }
+
+            if (d < g || h < g) {
+                g = (d > h ? h + 1 : d + 1);
+            }
+            else {
+                if (e4 !== k) {
+                    g++;
+                }
+            }
+            p[y] = h = g;
+            g = d;
+            d = b;
+            b = c;
+            c = a;
+        }
+    }
+
+    for (; x < m;) {
+        var e = t.charCodeAt(x);
+        c = x;
+        d = ++x;
+        for (y = 0; y < n; y++) {
+            a = p[y];
+            if (a < c || d < c) {
+                d = (a > d ? d + 1 : a + 1);
+            }
+            else {
+                if (e !== s.charCodeAt(y)) {
+                    d = c + 1;
+                }
+                else {
+                    d = c;
+                }
+            }
+            p[y] = d;
+            c = a;
+        }
+        h = d;
+    }
+
+    return h;
 }
